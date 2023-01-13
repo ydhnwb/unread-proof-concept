5c47e1bf286272fa933d11b7ef3b8740846b740f
Prieyuda Akadita S <ydhnwb-pc@pop-os.localdomain>
2023-01-13 16:36:00 +0700

    initial commit

---
+++ datas.js
@@ -0,0 +1,73 @@
+const comments = [
+    {
+        "_id": "1",
+        "isDeleted": false,
+        "mentions": [],
+        "replies": [],
+        "message": "Comment dengan id 1",
+        "type": "Task",
+        "module": "63bd33f29b789000087be89d",
+        "createdDate": "2023-01-11T13:36:22.872Z",
+        "updatedDate": "2023-01-11T13:36:22.872Z",
+    },
+    {
+        "_id": "2",
+        "isDeleted": false,
+        "mentions": [],
+        "replies": [],
+        "message": "Comment dengan id 2",
+        "type": "Task",
+        "module": "63bd33f29b789000087be89d",
+        "createdDate": "2023-01-11T13:36:22.872Z",
+        "updatedDate": "2023-01-11T13:36:22.872Z",
+    },
+
+    {
+        "_id": "3",
+        "isDeleted": false,
+        "mentions": [],
+        "replies": [],
+        "message": "Comment dengan id 3",
+        "type": "Task",
+        "module": "63bd33f29b789000087be89d",
+        "createdDate": "2023-01-11T13:36:22.872Z",
+        "updatedDate": "2023-01-11T13:36:22.872Z",
+    },
+
+    {
+        "_id": "4",
+        "isDeleted": false,
+        "mentions": [],
+        "replies": [],
+        "message": "Comment dengan id 4",
+        "type": "Task",
+        "module": "63bd33f29b789000087be89d",
+        "createdDate": "2023-01-11T13:36:22.872Z",
+        "updatedDate": "2023-01-11T13:36:22.872Z",
+    },
+    {
+        "_id": "5",
+        "isDeleted": false,
+        "mentions": [],
+        "replies": [],
+        "message": "Comment dengan id 5",
+        "type": "Task",
+        "module": "63bd33f29b789000087be89d",
+        "createdDate": "2023-01-11T13:36:22.872Z",
+        "updatedDate": "2023-01-11T13:36:22.872Z",
+    },
+
+    {
+        "_id": "6",
+        "isDeleted": false,
+        "mentions": [],
+        "replies": [],
+        "message": "Comment dengan id 6",
+        "type": "Task",
+        "module": "63bd33f29b789000087be89d",
+        "createdDate": "2023-01-11T13:36:22.872Z",
+        "updatedDate": "2023-01-11T13:36:22.872Z",
+    },
+]
+
+module.exports = { comments }
---
+++ index.js
@@ -0,0 +1,46 @@
+const { comments: c } = require('./datas')
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const port = 3000
+
+
+
+let comments = c
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/get-comments', (req, res) => {
+    res.send(comments)
+})
+
+io.on('connect', (socket) => {
+    const userId = socket.handshake.query["userId"]
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('[BE]: user disconnected');
+    });
+
+    socket.on('setSeen', (msg) => {
+        console.log('message: ' + msg + ' with userId' + socket.handshake.query['userId']);
+        const itemIdx = comments.findIndex((c) => c._id == msg)
+        console.log(itemIdx)
+        if (itemIdx != -1) {
+            const seenBy = comments[itemIdx].seenBy != undefined ? comments[itemIdx].seenBy : []
+            seenBy.push(userId)
+            Object.assign(comments[itemIdx], { seenBy })
+        }
+
+    });
+});
+
+server.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
