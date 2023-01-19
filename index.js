cda8c2a312363c7b394def7579a8bb4969145d29
Prieyuda Akadita S <akaditasustono@gmail.com>
2023-01-19 13:38:49 +0700

    add more cors for web implementation

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001'];
+
 const { comments: c } = require('./datas')
 const express = require('express');
+const cors = require('cors')
 const app = express();
+app.use(cors({
+    origin: function (origin, callback) {
+        // allow requests with no origin 
+        // (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}))
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
-const port = 3000
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
+});
+const port = 3001
 
 
 //database mock
